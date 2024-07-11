9937b07693c034acb8d8ad440893a1b03d9f44eb
Daniel Nunes <danielnunespk@gmail.com>
2024-07-11 13:44:41 -0300

    Push inicial

---
+++ module-helllo/src/main/java/br/com/danielnunesro/entities/Hello.java
@@ -0,0 +1,11 @@
+package br.com.danielnunesro.entities;
+
+
+public class Hello {
+
+    private String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+}
---
+++ sub-project-1/src/main/java/br/com/danielnunesro/Teste.java
@@ -0,0 +1,10 @@
+package br.com.danielnunesro;
+
+import br.com.danielnunesro.entities.Hello;
+
+public class Teste extends Hello {
+
+    public Teste(String name) {
+        super(name);
+    }
+}
